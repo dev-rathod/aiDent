49756c472b65b729cfacf7e3e6cb5aa805e4e6db
Krigupt <123670605+Krigupt@users.noreply.github.com>
2024-04-28 04:12:10 -0700

    save uploaded image

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from flask import Flask
+from flask import Flask,render_template,request
+import os
+
 
 app = Flask(__name__)
-
-@app.route("/")
+@app.route("/upload",methods=['GET', 'POST'])
 def index():
-    return "Testing Landing!"
+
+    if request.method=="POST":
+        # file = request.files['image']
+        # upload_folder = '/Users/krishna/Desktop/aiDent/images'
+        # os.makedirs(upload_folder, exist_ok=True)
+        # file_path = os.path.join(upload_folder, file.filename)
+        # file.save(file_path)
+        file = request.files['image']
+        upload_folder = '/Users/krishna/Desktop/aiDent/static/images'
+        os.makedirs(upload_folder, exist_ok=True)
+        file_path = os.path.join(upload_folder, file.filename)
+        file.save(file_path)
+
+        # Optionally, you can return a response to the user
+        return "File uploaded successfully"
+
+    return render_template("input.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True,port=5300)
+
+
